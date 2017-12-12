7e3989276a9f9f58409ae14d82db6a5aac334f17
YiliaYingysss ZhengYil <virginzyl@gmail.com>
2017-12-12 17:15:40 +1100

    add api and client app

---
+++ IdentityServer.Clients.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace IdentityServer.Clients.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+
+        private static async Task MainTaskAsync()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                System.Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client","secret");
+        }
+    }
+}

07b260047c22894e68594582da8146ee0bc0bd43
YiliaYingysss ZhengYil <virginzyl@gmail.com>
2017-12-12 12:41:57 +1100

    IdentityServer Init commit

---
+++ IdentityServer/Config/Config.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Config
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResource()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1","First API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedScopes = {"api1"}
+                }
+            };
+        }
+    }
+}
